96a4b574c53b8add354b9d8146958ecd92142f1e
piagiovannelli <101904347+piagiovannelli@users.noreply.github.com>
2022-10-12 16:05:38 +0200

    .

--- Aufgaben/L01_Zufallsgedicht/L01.js
+++ Aufgaben/L01_Zufallsgedicht/L01.js
@@ -1,8 +1,28 @@
-var AufgabeZufallsgedicht;
-(function (AufgabeZufallsgedicht) {
+//Programmstruktur implementieren
+var zufallsgedicht;
+(function (zufallsgedicht) {
+    //Arrays
     let s = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
     let p = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
     let o = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
     console.log(s, p, o);
-})(AufgabeZufallsgedicht || (AufgabeZufallsgedicht = {}));
+    //For-Schleife
+    for (let index = 6; index >= 1; index--) {
+        let letzterSatz = getVerse(s, p, o);
+        console.log(letzterSatz);
+        //console.log(index);
+        //getVerse(s, p, o);
+        //console.log(getVerse);
+    }
+    function getVerse(_s, _p, _o) {
+        let randoms = Math.floor(Math.random() * _s.length);
+        let randomp = Math.floor(Math.random() * _s.length);
+        let randomo = Math.floor(Math.random() * _s.length);
+        let verse = _s[randoms] + " " + _p[randomp] + " " + _o[randomo] + " .";
+        _s.splice(randoms, 1);
+        _p.splice(randomp, 1);
+        _o.splice(randomo, 1);
+        return verse;
+    }
+})(zufallsgedicht || (zufallsgedicht = {}));
 //# sourceMappingURL=L01.js.map
--- Aufgaben/L01_Zufallsgedicht/L01.ts
+++ Aufgaben/L01_Zufallsgedicht/L01.ts
@@ -1,15 +1,39 @@
-namespace AufgabeZufallsgedicht{
+//Programmstruktur implementieren
+namespace zufallsgedicht {
+    //Arrays
+    let s: string[] = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
+    let p: string[] = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
+    let o: string[] = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
+
+    console.log(s, p, o);
+
+    //For-Schleife
+
+    for (let index: number = 6; index >= 1; index--) {
+        let letzterSatz: string = getVerse(s, p, o);
+        console.log(letzterSatz);
+
+        //console.log(index);
+        //getVerse(s, p, o);
+        //console.log(getVerse);
+
+    }
 
 
-let s: string[] = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
-let p: string[] = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
-let o: string[] = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
+    function getVerse(_s: string[], _p: string[], _o: string[]): string {
+        let randoms: number = Math.floor(Math.random() * _s.length);
+        let randomp: number = Math.floor(Math.random() * _s.length);
+        let randomo: number = Math.floor(Math.random() * _s.length);
 
-console.log(s,p,o);
+        let verse: string = _s[randoms] + " " + _p[randomp] + " " + _o[randomo] + " .";
+
+        _s.splice(randoms, 1);
+        _p.splice(randomp, 1);
+        _o.splice(randomo, 1);
+
+        return verse;
+    }
 
 
 
 }
-
-
-
