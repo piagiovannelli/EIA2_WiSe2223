ac42e6dbec2e3a5cec70e7d98da3836b1b718f57
piagiovannelli <101904347+piagiovannelli@users.noreply.github.com>
2022-10-12 15:10:20 +0200

    .

---
+++ Aufgaben/L01_Zufallsgedicht/L01.js
@@ -0,0 +1,8 @@
+var AufgabeZufallsgedicht;
+(function (AufgabeZufallsgedicht) {
+    let s = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
+    let p = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
+    let o = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
+    console.log(s, p, o);
+})(AufgabeZufallsgedicht || (AufgabeZufallsgedicht = {}));
+//# sourceMappingURL=L01.js.map
---
+++ Aufgaben/L01_Zufallsgedicht/L01.ts
@@ -0,0 +1,15 @@
+namespace AufgabeZufallsgedicht{
+
+
+let s: string[] = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
+let p: string[] = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
+let o: string[] = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
+
+console.log(s,p,o);
+
+
+
+}
+
+
+
