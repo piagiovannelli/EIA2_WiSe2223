bf37a1d2d6f93b31cde6b276ff03953fd2bec997
piagiovannelli <101904347+piagiovannelli@users.noreply.github.com>
2022-11-18 14:38:37 +0100

    .

--- Aufgaben/L06_DatabaseServer/L06.js
+++ Aufgaben/L06_DatabaseServer/L06.js
@@ -1,17 +1,17 @@
-
 var shoppinglistA06;
 (function (shoppinglistA06) {
     window.addEventListener("load", handleLoad);
+    //interface für die Daten in der JSON File
+    let counter = 0;
+    let globalBol = false;
     //lädt Liste und ruft loadData auf
     async function handleLoad() {
         let button = document.querySelector("button[id=but1]");
         let response = await fetch("https://webuser.hs-furtwangen.de/~aberleba/Database/index.php/?command=find&collection=data");
         let entry = await response.text();
         let data = JSON.parse(entry);
-        console.log(data);
-        console.log(data[0].Item);
         button.addEventListener("click", handleButton);
-        // loadData(data);
+        loadData(data);
         // clearInputs();
     }
     //managed die Buttons
@@ -30,34 +30,51 @@ var shoppinglistA06;
                 json[key] = values.length > 1 ? values : values[0];
             }
         let query = new URLSearchParams();
+        let newJSON = JSON.stringify(json);
+        console.log(newJSON);
         query.set("command", "insert");
         query.set("collection", "data");
-        query.set("data", JSON.stringify(json));
-        console.log(JSON.stringify(json));
+        query.set("data", newJSON);
+        // console.log(JSON.stringify(json));  
         let response = await fetch("https://webuser.hs-furtwangen.de/~aberleba/Database/index.php?" + query.toString());
         // console.log(response);
-        // console.log("data sent");
+        console.log("data sent");
     }
     //lädt die Daten aus dem JSON Array in Variablen und gibt sie an loadItem weiter
-    function loadData(data) {
-        console.log("loadData");
-        for (let index = 0; index < data.length; index++) {
-            let item = data[index].Item;
-            let amount = data[index].Amount;
-            // let date: string = data[index].date;
-            let comment = data[index].Area;
-            // let purchase: string = data[index].purchase;
-            console.log(item + amount + comment);
-            loadItem(item, amount, comment);
+    function loadData(newData) {
+        let list = [];
+        for (let test in newData.data) {
+            list.push(test);
         }
+        for (let index of list) {
+            console.log(newData.data[index].Item);
+            let item = newData.data[index].Item;
+            let amount = newData.data[index].Amount;
+            let date = newData.data[index].NewDate.toString();
+            let comment = newData.data[index].Area;
+            let purchaseCheckbox = newData.data[index].Checkbox;
+            // console.log(purchaseCheckbox);
+            let purchase;
+            if (purchaseCheckbox == "on") {
+                purchase = " buy";
+                // console.log("purchase=" + purchase);
+            }
+            else {
+                purchase = "";
+            }
+            loadItem(item, amount, comment, purchase, date, index);
+            counter++;
+        }
+        // loadData(newData);
     }
     //lädt den Input in den Feldern in Variablen und übergibt es dann zur loadItem Funktion
     function loadInput() {
         let formData = new FormData(document.forms[0]);
         let item = formData.get("Item").toString();
         let amount = formData.get("Amount").toString();
-        let date = new Date().toLocaleDateString();
+        let date = formData.get("NewDate").toString();
         let comment = formData.get("Area").toString();
+        let index;
         //umwandlung nextPurchase von Input in string
         let purchaseCheckbox = formData.get("Checkbox");
         let purchase = "";
@@ -68,23 +85,24 @@ var shoppinglistA06;
             purchase = " buy";
         }
         //generiere nun einen neuen Eintrag
-        loadItem(item, amount, comment);
+        loadItem(item, amount, comment, purchase, date, index);
         //löscht Value von Inputs
         // clearInputs();
     }
     //Funktion zur generierung eines Item Felds im Output
-    function loadItem(item, amount, comment) {
+    function loadItem(item, amount, comment, purchase, date, index) {
         let newDiv = document.createElement("div");
         newDiv.id = "createDiv";
         let parent = document.querySelector("#output");
         newDiv.className = "genoutput";
-        newDiv.innerHTML = amount + " " + item + " " + comment;
+        newDiv.innerHTML = date + " " + amount + " " + item + " " + comment + " " + purchase;
         parent.appendChild(newDiv);
         let newContainer = document.createElement("div");
         newContainer.id = "containerIcons";
         newDiv.appendChild(newContainer);
         let newCheckbox = document.createElement("input");
         newCheckbox.type = "checkbox";
+        newCheckbox.name = "Checkbox";
         newContainer.appendChild(newCheckbox);
         let newEdit = document.createElement("div");
         newEdit.innerHTML = "<img id='edit' src='./pen-solid.svg'>";
@@ -94,25 +112,79 @@ var shoppinglistA06;
         newCheckbox.id = "trash";
         newContainer.appendChild(newTrash);
         newEdit.addEventListener("click", function () {
-            editItem(newDiv, item, amount, comment);
+            editItem(newDiv, item, amount, comment, date, index);
         });
         newTrash.addEventListener("click", function () {
-            deleteItem(newDiv);
+            deleteItem(newDiv, index);
+        });
+        newCheckbox.addEventListener("click", function () {
+            updateDate(newDiv, index, item, amount, comment, date, purchase);
         });
     }
+    async function updateDate(newDiv, index, item, amount, comment, date, purchase) {
+        console.log("checkbox");
+        var dateObj = new Date();
+        var month = dateObj.getUTCMonth() + 1; //months from 1-12
+        var day = dateObj.getUTCDate();
+        var year = dateObj.getUTCFullYear();
+        var NewDate = year + "-" + month + "-" + day;
+        newDiv.innerHTML = NewDate + " " + amount + " " + item + " " + comment + " " + purchase;
+        let newContainer = document.createElement("div");
+        newContainer.id = "containerIcons";
+        newDiv.appendChild(newContainer);
+        let newCheckbox = document.createElement("input");
+        newCheckbox.type = "checkbox";
+        newCheckbox.name = "Checkbox";
+        newContainer.appendChild(newCheckbox);
+        let newEdit = document.createElement("div");
+        newEdit.innerHTML = "<img id='edit' src='./pen-solid.svg'>";
+        newContainer.appendChild(newEdit);
+        let newTrash = document.createElement("div");
+        newTrash.innerHTML = "<img id='trash' src='./trash-solid.svg'>";
+        newCheckbox.id = "trash";
+        newContainer.appendChild(newTrash);
+        newEdit.addEventListener("click", function () {
+            editItem(newDiv, item, amount, comment, date, index);
+        });
+        newTrash.addEventListener("click", function () {
+            deleteItem(newDiv, index);
+        });
+        newCheckbox.addEventListener("click", function () {
+            updateDate(newDiv, index, item, amount, comment, date, purchase);
+        });
+        let json = { NewDate };
+        let newJSON = JSON.stringify(json);
+        let query = new URLSearchParams();
+        query.set("command", "update");
+        query.set("collection", "data");
+        query.set("id", index);
+        console.log(index + "index");
+        query.set("data", newJSON);
+        console.log(query);
+        let response = await fetch("https://webuser.hs-furtwangen.de/~aberleba/Database/index.php?" + query.toString());
+        console.log("data sent");
+    }
     //löscht ein Item bei click auf trash
-    function deleteItem(newDiv) {
+    async function deleteItem(newDiv, index) {
         newDiv.parentElement.removeChild(newDiv);
+        let query = new URLSearchParams();
+        query.set("command", "delete");
+        query.set("collection", "data");
+        query.set("id", index.toString());
+        let response = await fetch("https://webuser.hs-furtwangen.de/~aberleba/Database/index.php?" + query.toString());
+        console.log("deletet");
     }
     //editiert ein Item bei click auf edit
-    function editItem(newDiv, item, amount, comment) {
+    async function editItem(newDiv, item, amount, comment, date, index) {
         let itemx = document.querySelector("input#inputx");
         itemx.value = item;
         let amountx = document.querySelector("input#amountx");
         amountx.value = amount.toString();
         let commentx = document.querySelector("input#commentx");
         commentx.value = comment;
-        deleteItem(newDiv);
+        let datex = document.querySelector("input#datex");
+        datex.value = date;
+        deleteItem(newDiv, index);
     }
     // cleared die Input Felder
     function clearInputs() {
--- Aufgaben/L06_DatabaseServer/L06.ts
+++ Aufgaben/L06_DatabaseServer/L06.ts
@@ -1,20 +1,26 @@
-
-
 namespace shoppinglistA06 {
 
     window.addEventListener("load", handleLoad);
 
     //interface für die Daten in der JSON File
 
+    let counter: number = 0;
+    let globalBol: boolean = false;
 
-    interface TestData {
+    interface Item {
         Item: string;
         Amount: string;
+        Checkbox: string;
         Area: string;
+        NewDate: string;
     }
 
     interface FormDataJSON {
         [key: string]: FormDataEntryValue | FormDataEntryValue[];
+    }
+
+    interface Entrys {
+        [category: string]: Item[];
     }
 
 
@@ -24,16 +30,13 @@ namespace shoppinglistA06 {
 
         let response: Response = await fetch("https://webuser.hs-furtwangen.de/~aberleba/Database/index.php/?command=find&collection=data");
         let entry: string = await response.text();
-        let data: TestData[] = JSON.parse(entry);
-
-        console.log(data);
-        console.log(data[0].Item);
+        let data: Entrys = JSON.parse(entry);
 
 
         button.addEventListener("click", handleButton);
 
         
-        // loadData(data);
+        loadData(data);
         // clearInputs();
     }
 
@@ -55,30 +58,51 @@ namespace shoppinglistA06 {
             let values: FormDataEntryValue[] = formData.getAll(key);
             json[key] = values.length > 1 ? values : values[0];
         }
-
+      
+        
         let query: URLSearchParams = new URLSearchParams();
+        let newJSON: string = JSON.stringify(json);
+        console.log(newJSON);
         query.set("command", "insert");
         query.set("collection", "data");
-        query.set("data", JSON.stringify(json));  
-        console.log(JSON.stringify(json));  
+        query.set("data", newJSON);  
+        // console.log(JSON.stringify(json));  
         let response: Response = await fetch("https://webuser.hs-furtwangen.de/~aberleba/Database/index.php?" + query.toString());
         // console.log(response);
-        // console.log("data sent");
+        console.log("data sent");
     }
 
 
     //lädt die Daten aus dem JSON Array in Variablen und gibt sie an loadItem weiter
-    function loadData(data: TestData[]): void {
-        console.log("loadData");
-        for (let index: number = 0; index < data.length; index++) {
-            let item: string = data[index].Item;
-            let amount: string = data[index].Amount;
-            // let date: string = data[index].date;
-            let comment: string = data[index].Area;
-            // let purchase: string = data[index].purchase;
-            console.log(item + amount + comment);
-            loadItem(item, amount, comment);
+    function loadData(newData: Entrys): void {
+    
+        let list: any[] = [];
+        for (let test in newData.data) {
+            list.push(test);
         }
+
+
+
+        for (let index of list) {
+            console.log(newData.data[index].Item);
+            let item: string = newData.data[index].Item;
+            let amount: string = newData.data[index].Amount;
+            let date: string = newData.data[index].NewDate.toString();
+            let comment: string = newData.data[index].Area;
+            let purchaseCheckbox: string = newData.data[index].Checkbox;
+            // console.log(purchaseCheckbox);
+            let purchase: string;
+            if (purchaseCheckbox == "on") {
+                purchase = " buy";
+                // console.log("purchase=" + purchase);
+            } else {
+                purchase = "";
+            }
+            loadItem(item, amount, comment, purchase, date, index);
+            counter++;
+          
+        }
+        // loadData(newData);
     }
 
     //lädt den Input in den Feldern in Variablen und übergibt es dann zur loadItem Funktion
@@ -86,8 +110,9 @@ namespace shoppinglistA06 {
         let formData: FormData = new FormData(document.forms[0]);
         let item: string = formData.get("Item").toString();
         let amount: string = formData.get("Amount").toString();
-        let date: string = new Date().toLocaleDateString();
+        let date: string = formData.get("NewDate").toString();
         let comment: string = formData.get("Area").toString();
+        let index: any;
 
         //umwandlung nextPurchase von Input in string
         let purchaseCheckbox: FormDataEntryValue = formData.get("Checkbox");
@@ -101,19 +126,21 @@ namespace shoppinglistA06 {
 
 
         //generiere nun einen neuen Eintrag
-        loadItem(item, amount, comment);
+        loadItem(item, amount, comment, purchase, date, index);
 
         //löscht Value von Inputs
         // clearInputs();
     }
 
     //Funktion zur generierung eines Item Felds im Output
-    function loadItem(item: string, amount: string, comment: string): void {
+    function loadItem(item: string, amount: string, comment: string, purchase: string, date: string, index: any): void {
         let newDiv: HTMLDivElement = document.createElement("div");
+       
         newDiv.id = "createDiv";
         let parent: Element = document.querySelector("#output");
         newDiv.className = "genoutput";
-        newDiv.innerHTML = amount + " " + item + " " + comment;
+        newDiv.innerHTML = date + " " + amount + " " + item + " " + comment + " " + purchase;
+     
         parent.appendChild(newDiv);
 
         let newContainer: HTMLDivElement = document.createElement("div");
@@ -122,6 +149,7 @@ namespace shoppinglistA06 {
 
         let newCheckbox: HTMLInputElement = document.createElement("input");
         newCheckbox.type = "checkbox";
+        newCheckbox.name = "Checkbox";
         newContainer.appendChild(newCheckbox);
 
         let newEdit: HTMLDivElement = document.createElement("div");
@@ -134,28 +162,110 @@ namespace shoppinglistA06 {
         newContainer.appendChild(newTrash);
 
         newEdit.addEventListener("click", function (): void {
-            editItem(newDiv, item, amount, comment);
+            editItem(newDiv, item, amount, comment, date, index);
         });
 
         newTrash.addEventListener("click", function (): void {
-            deleteItem(newDiv);
+            deleteItem(newDiv, index);
+        });
+
+        newCheckbox.addEventListener("click", function (): void {
+            updateDate(newDiv, index, item, amount, comment, date, purchase);
         });
     }
 
+    async function updateDate(newDiv: HTMLDivElement, index: any, item: string, amount: string, comment: string, date: string, purchase: string): Promise<void> {
+            console.log("checkbox");
+            var dateObj: Date = new Date();
+            var month: number = dateObj.getUTCMonth() + 1; //months from 1-12
+            var day: number = dateObj.getUTCDate();
+            var year: number = dateObj.getUTCFullYear();
+
+
+            var NewDate: string = year + "-" + month + "-" + day;  
+           
+            newDiv.innerHTML = NewDate + " " + amount + " " + item + " " + comment + " " + purchase;
+
+            let newContainer: HTMLDivElement = document.createElement("div");
+            newContainer.id = "containerIcons";
+            newDiv.appendChild(newContainer);
+    
+            let newCheckbox: HTMLInputElement = document.createElement("input");
+            newCheckbox.type = "checkbox";
+            newCheckbox.name = "Checkbox";
+            newContainer.appendChild(newCheckbox);
+    
+            let newEdit: HTMLDivElement = document.createElement("div");
+            newEdit.innerHTML = "<img id='edit' src='./pen-solid.svg'>";
+            newContainer.appendChild(newEdit);
+    
+            let newTrash: HTMLDivElement = document.createElement("div");
+            newTrash.innerHTML = "<img id='trash' src='./trash-solid.svg'>";
+            newCheckbox.id = "trash";
+            newContainer.appendChild(newTrash);
+    
+            newEdit.addEventListener("click", function (): void {
+                editItem(newDiv, item, amount, comment, date, index);
+            });
+    
+            newTrash.addEventListener("click", function (): void {
+                deleteItem(newDiv, index);
+            });
+    
+            newCheckbox.addEventListener("click", function (): void {
+                updateDate(newDiv, index, item, amount, comment, date, purchase);
+            });
+            let json: FormDataJSON = {NewDate};
+            let newJSON: string = JSON.stringify(json);
+            
+            let query: URLSearchParams = new URLSearchParams();
+            query.set("command", "update");
+            query.set("collection", "data");
+            query.set("id", index);
+            console.log(index + "index");  
+            query.set("data", newJSON   );
+            console.log(query);
+        
+            
+
+            let response: Response = await fetch("https://webuser.hs-furtwangen.de/~aberleba/Database/index.php?" + query.toString());
+            console.log("data sent");
+
+    }
     //löscht ein Item bei click auf trash
-    function deleteItem(newDiv: HTMLDivElement): void {
+    async function deleteItem(newDiv: HTMLDivElement, index: any): Promise<void> {
         newDiv.parentElement.removeChild(newDiv);
+        let query: URLSearchParams = new URLSearchParams();
+       
+      
+        query.set("command", "delete");
+        query.set("collection", "data");
+        query.set("id", index.toString());  
+
+        let response: Response = await fetch("https://webuser.hs-furtwangen.de/~aberleba/Database/index.php?" + query.toString());
+
+        console.log("deletet");
+
     }
 
     //editiert ein Item bei click auf edit
-    function editItem(newDiv: HTMLDivElement, item: string, amount: string, comment: string): void {
+    async function editItem(newDiv: HTMLDivElement, item: string, amount: string, comment: string, date: string, index: any): Promise<void> {
         let itemx: HTMLInputElement = document.querySelector("input#inputx");
         itemx.value = item;
         let amountx: HTMLInputElement = document.querySelector("input#amountx");
         amountx.value = amount.toString();
         let commentx: HTMLInputElement = document.querySelector("input#commentx");
         commentx.value = comment;
-        deleteItem(newDiv);
+        let datex: Element = document.querySelector("input#datex");
+        datex.value = date;
+
+   
+
+      
+        deleteItem(newDiv, index); 
+
+
+
     }
 
     // cleared die Input Felder
@@ -168,3 +278,4 @@ namespace shoppinglistA06 {
         commentx.value = "";
     }
 }
+
